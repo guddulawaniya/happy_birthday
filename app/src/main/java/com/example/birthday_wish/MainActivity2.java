f2b7c3e1c6a8e5cffeb4a54050673095ab641029
guddu <guddulawaniya123@gmail.com>
2022-05-18 16:44:17 +0530

    commit

--- app/src/main/java/com/example/birthday_wish/MainActivity2.java
+++ app/src/main/java/com/example/birthday_wish/MainActivity2.java
@@ -6,24 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private View name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button button = findViewById(R.id.button);
+        EditText name = findViewById(R.id.nameinput);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().trim().length()!=0)
+                {
+
+                    String input = name.getText().toString();
+
+                    Toast.makeText(MainActivity2.this, "Button Was Clicked "+input, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                    intent.putExtra("sendsms",input);
+                    startActivity(intent);
+
+                }
+                else
+                {
+
+                    Toast.makeText(MainActivity2.this, "Enter The Name ", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
     }
 
-    public void createbithrdaybutton(View view) {
-        name = findViewById(R.id.nameinput);
-        String input = name.toString();
-        Toast.makeText(this, "button was clicked $name", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this,MainActivity.class);
-        intent.putExtra("name", String.valueOf(name));
-        startActivity(intent);
-    }
 }
